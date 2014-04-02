e43ece3d9e0750ac6115442784e8c6cb7c33a862
Alexander Bulaev <alexbool@yandex-team.ru>
2014-04-02 17:18:31 +0400

    Extract abstract class

---
+++ src/main/java/me/alexbool/offheap/AbstractInt2SomethingOpenHashMap.java
@@ -0,0 +1,90 @@
+package me.alexbool.offheap;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.util.Map;
+
+import it.unimi.dsi.fastutil.HashCommon;
+
+/**
+ * @author alexbool
+ */
+public abstract class AbstractInt2SomethingOpenHashMap<T> implements Map<Integer, T> {
+
+    protected static final int SIZE_OF_INT = 4;
+
+    protected final IntBuffer keys;
+    protected final ByteBuffer used;
+    protected final float f;
+    protected final int n;
+    protected int size;
+    protected int mask;
+
+    protected AbstractInt2SomethingOpenHashMap(int expected) {
+        this.f = 0.75f;
+        this.n = HashCommon.arraySize(expected, f);
+        this.mask = n - 1;
+        this.keys = ByteBuffer.allocateDirect(n * SIZE_OF_INT).asIntBuffer();
+        this.used = ByteBuffer.allocateDirect((int) Math.ceil((double) n / 8));
+        this.size = 0;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        if (key instanceof Integer) {
+            return containsKey(((Integer) key).intValue());
+        } else {
+            return false;
+        }
+    }
+
+    public boolean containsKey(int k) {
+        // The starting point.
+        int pos = (HashCommon.murmurHash3((k) ^ mask)) & mask;
+        // There's always an unused entry.
+        while (isUsedAt(pos)) {
+            if (getKeyAt(pos) == k) return true;
+            pos = (pos + 1) & mask;
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    protected boolean isUsedAt(int pos) {
+        int index = pos / 8;
+        byte bits = used.get(index);
+        byte bitMask = (byte) (1 << (pos % 8));
+        return (bits & bitMask) != 0;
+    }
+
+    protected void setUsedAt(int pos) {
+        int index = pos / 8;
+        byte bits = used.get(index);
+        byte bitMask = (byte) (1 << (pos % 8));
+        used.put(index, (byte) (bits | bitMask));
+    }
+
+    protected int getKeyAt(int pos) {
+        return keys.get(pos);
+    }
+
+    protected void setKeyAt(int pos, int key) {
+        keys.put(pos, key);
+    }
+
+    @Override
+    public void putAll(Map<? extends Integer, ? extends T> m) {
+        for (Map.Entry<? extends Integer, ? extends T> e : m.entrySet()) {
+            put(e.getKey(), e.getValue());
+        }
+    }
+}
--- src/main/java/me/alexbool/offheap/Int2IntArrayOpenHashMap.java
+++ src/main/java/me/alexbool/offheap/Int2IntArrayOpenHashMap.java
@@ -3,67 +3,23 @@ package me.alexbool.offheap;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.util.Collection;
-import java.util.Map;
 import java.util.Set;
 
 import it.unimi.dsi.fastutil.HashCommon;
 
-public class Int2IntArrayOpenHashMap implements Map<Integer, int[]> {
+public class Int2IntArrayOpenHashMap extends AbstractInt2SomethingOpenHashMap<int[]> {
 
-    protected static final int SIZE_OF_INT = 4;
-
-    protected final IntBuffer keys;
     protected final IntBuffer valueOffsets;
     protected final IntBuffer valueSizes;
     protected final IntBuffer values;
-    protected final ByteBuffer used;
-    protected final float f;
-    protected final int n;
-    protected int size;
-    protected transient int mask;
     protected int maxOffset;
 
     public Int2IntArrayOpenHashMap(int expected, int totalValues) {
-        this.f = 0.75f;
-        this.n = HashCommon.arraySize(expected, f);
-        this.mask = n - 1;
-        this.keys = ByteBuffer.allocateDirect(n * SIZE_OF_INT).asIntBuffer();
+        super(expected);
         this.valueOffsets = ByteBuffer.allocateDirect(n * SIZE_OF_INT).asIntBuffer();
         this.valueSizes = ByteBuffer.allocateDirect(n * SIZE_OF_INT).asIntBuffer();
         this.values = ByteBuffer.allocateDirect(totalValues * SIZE_OF_INT).asIntBuffer();
-        this.used = ByteBuffer.allocateDirect((int) Math.ceil((double) n / 8));
-        this.size = 0;
         this.maxOffset = 0;
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    @Override
-    public boolean containsKey(Object key) {
-        if (key instanceof Integer) {
-            return containsKey(((Integer) key).intValue());
-        } else {
-            return false;
-        }
-    }
-
-    public boolean containsKey(int k) {
-        // The starting point.
-        int pos = (HashCommon.murmurHash3((k) ^ mask)) & mask;
-        // There's always an unused entry.
-        while (isUsedAt(pos)) {
-            if (getKeyAt(pos) == k) return true;
-            pos = (pos + 1) & mask;
-        }
-        return false;
     }
 
     @Override
@@ -114,13 +70,6 @@ public class Int2IntArrayOpenHashMap implements Map<Integer, int[]> {
     }
 
     @Override
-    public void putAll(Map<? extends Integer, ? extends int[]> m) {
-        for (Map.Entry<? extends Integer, ? extends int[]> e : m.entrySet()) {
-            put(e.getKey(), e.getValue());
-        }
-    }
-
-    @Override
     public int[] remove(Object key) {
         throw new UnsupportedOperationException("Remove is not supported");
     }
@@ -143,28 +92,6 @@ public class Int2IntArrayOpenHashMap implements Map<Integer, int[]> {
     @Override
     public Set<Entry<Integer, int[]>> entrySet() {
         throw new UnsupportedOperationException("entrySet is not supported");
-    }
-
-    protected boolean isUsedAt(int pos) {
-        int index = pos / 8;
-        byte bits = used.get(index);
-        byte bitMask = (byte) (1 << (pos % 8));
-        return (bits & bitMask) != 0;
-    }
-
-    protected void setUsedAt(int pos) {
-        int index = pos / 8;
-        byte bits = used.get(index);
-        byte bitMask = (byte) (1 << (pos % 8));
-        used.put(index, (byte) (bits | bitMask));
-    }
-
-    protected int getKeyAt(int pos) {
-        return keys.get(pos);
-    }
-
-    protected void setKeyAt(int pos, int key) {
-        keys.put(pos, key);
     }
 
     protected int[] getValueAt(int pos) {
