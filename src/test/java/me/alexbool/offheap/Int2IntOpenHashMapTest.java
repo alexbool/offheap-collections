4430344075a82c14d8fdcfe78875791421f67e46
Alexander Bulaev <alexbool@yandex-team.ru>
2014-04-03 12:02:53 +0400

    More tests

--- src/test/java/me/alexbool/offheap/Int2IntOpenHashMapTest.java
+++ src/test/java/me/alexbool/offheap/Int2IntOpenHashMapTest.java
@@ -1,5 +1,8 @@
 package me.alexbool.offheap;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -18,5 +21,41 @@ public class Int2IntOpenHashMapTest {
         for (int i = 0; i < 10; i++) {
             Assert.assertEquals(i + 1, map.get(i));
         }
+    }
+
+    @Test
+    public void containsKey() {
+        Int2IntOpenHashMap map = new Int2IntOpenHashMap(2);
+        Assert.assertFalse(map.containsKey(1));
+        map.put(1, 200);
+        Assert.assertTrue(map.containsKey(1));
+    }
+
+    @Test
+    public void bigMap() {
+        final int mapSize = 500000;
+        Int2IntOpenHashMap map = new Int2IntOpenHashMap(mapSize);
+        for (int i = 0; i < mapSize; i++) {
+            map.put(i, i * 2);
+        }
+
+        for (int i = 0; i < mapSize; i++) {
+            Assert.assertEquals(i * 2, map.get(i));
+        }
+    }
+
+    @Test
+    public void putAll() {
+        Int2IntOpenHashMap map = new Int2IntOpenHashMap(2);
+        Map<Integer, Integer> otherMap = new HashMap<>();
+
+        otherMap.put(1, 10);
+        otherMap.put(2, 20);
+
+        map.putAll(otherMap);
+
+        Assert.assertEquals(2, map.size());
+        Assert.assertEquals(10, map.get(1));
+        Assert.assertEquals(20, map.get(2));
     }
 }
